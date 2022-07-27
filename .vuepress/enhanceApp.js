7c47c91767e672dc6aa957d891c6daf912deb980
fabianriewe <fabian@kyve.network>
2022-07-27 14:38:58 +0200

    fix: fixed WARP link #2

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -2,6 +2,6 @@ export default ({ router }) => {
   router.addRoutes([
     { path: "/getting-started/chain-node.html", redirect: "/validators/chain-node" },
     { path: "/getting-started/protocol-node.html", redirect: "/validators/protocol-node" },
-    { path: "/getting-started/accessing-data/", redirect: "/developers/accessing-data" },
+    { path: "/getting-started/accessing-data/", redirect: "/developers/accessing-data/basic-queries.html" },
   ]);
 };
