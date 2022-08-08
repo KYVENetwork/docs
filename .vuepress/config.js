1b5b74c6592118a14bdca251d496a6f13734e8bd
fabianriewe <fabian@kyve.network>
2022-08-08 14:17:47 +0200

    chore: added analytics

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -179,4 +179,24 @@ module.exports = {
       },
     ],
   ],
+  head: [
+    ['script', {}, `
+            (function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:3099667,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    `],
+    ['script', {async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-SY5FWZVWK2"}, ''],
+    ['script', {}, `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SY5FWZVWK2');
+    `]
+  ]
 };
