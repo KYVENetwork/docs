2e025435ae81087b2d115951bc90881b67cc8877
mbreithecker <m@breithecker.de>
2022-09-06 16:43:58 +0200

    chore: revert .vuepress

---
+++ .vuepress/enhanceApp.js
@@ -0,0 +1,16 @@
+export default ({ router }) => {
+  router.addRoutes([
+    {
+      path: '/getting-started/chain-node.html',
+      redirect: '/validators/chain-node',
+    },
+    {
+      path: '/getting-started/protocol-node.html',
+      redirect: '/validators/protocol-node',
+    },
+    {
+      path: '/getting-started/accessing-data/',
+      redirect: '/developers/accessing-data/basic-queries.html',
+    },
+  ]);
+};
