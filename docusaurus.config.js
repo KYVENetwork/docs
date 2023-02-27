e459a6df25de8884e358a29d3362483924e3e0a7
fabianriewe <fabian@kyve.network>
2023-02-27 14:13:27 +0100

    fix: fixed editing url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/KYVENetwork/docs",
+          editUrl: "https://github.com/KYVENetwork/docs/tree/main",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
