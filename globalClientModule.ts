1e9aecd1de78a37a5903c02a1d72f6bc6b372a4e
fabianriewe <fabian@kyve.network>
2023-09-14 15:44:22 +0200

    fix: improved code by refactoring into function

--- globalClientModule.ts
+++ globalClientModule.ts
@@ -6,12 +6,32 @@ import axios from "axios";
 const module: ClientModule = {
   onRouteDidUpdate({location, previousLocation}) {
     if (location.pathname != previousLocation?.pathname) {
-      // todo: put this into environment
-      axios.post(`https://apm.kyve.network/page`,
-        {user_id: null, path: window.location.href, referrer: document.referrer},
-        {withCredentials: true})
+      sendPageEvent();
     }
 
+  }
+}
+
+const AJS_ANONYMOUS_ID = 'ajs_anonymous_id';
+// todo fetch this from environment
+const ANALYTICS_URL = "https://apm.kyve.network";
+
+async function sendPageEvent(): Promise<void> {
+  const data = {
+    user_id: null,
+    path: window.location.href,
+    referrer: document.referrer,
+    ajs_anonymous_id: localStorage.getItem(AJS_ANONYMOUS_ID)
+  };
+
+  try {
+    const res = await axios.post(`${ANALYTICS_URL}/page`, data, {withCredentials: true});
+    const id = res.data[AJS_ANONYMOUS_ID];
+    if (id) {
+      localStorage.setItem(AJS_ANONYMOUS_ID, id);
+    }
+  } catch (error) {
+    console.error('There was an error sending identify data to APM:', error);
   }
 }
 
