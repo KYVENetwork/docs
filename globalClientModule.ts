ec355877f980526f8c7fdf935559a5ba08b2f6f4
Troy Kessler <troy.kessler99@gmail.com>
2023-11-23 14:25:51 +0100

    chore: edit events

--- globalClientModule.ts
+++ globalClientModule.ts
@@ -1,38 +1,68 @@
 // from https://github.com/facebook/docusaurus/issues/3399
 
-import {ClientModule} from "@docusaurus/types";
+import { ClientModule } from "@docusaurus/types";
 import axios from "axios";
 
+document.body.addEventListener("copy", function () {
+  const selection = document.getSelection();
+  sendCopyEvent(selection.toString());
+});
+
 const module: ClientModule = {
-  onRouteDidUpdate({location, previousLocation}) {
+  onRouteDidUpdate({ location, previousLocation }) {
     if (location.pathname != previousLocation?.pathname) {
       sendPageEvent();
     }
+  },
+};
 
-  }
-}
-
-const AJS_ANONYMOUS_ID = 'ajs_anonymous_id';
+const AJS_ANONYMOUS_ID = "ajs_anonymous_id";
 // todo fetch this from environment
 const ANALYTICS_URL = "https://apm.kyve.network";
+
+async function sendCopyEvent(content: string): Promise<void> {
+  const data = {
+    user_id: null,
+    ajs_anonymous_id: localStorage.getItem(AJS_ANONYMOUS_ID),
+    path: window.location.href,
+    referrer: document.referrer,
+    event: "COPY",
+    properties: {
+      content,
+    },
+  };
+  try {
+    const res = await axios.post(`${ANALYTICS_URL}/track`, data, {
+      withCredentials: true,
+    });
+    const id = res.data[AJS_ANONYMOUS_ID];
+    if (id) {
+      localStorage.setItem(AJS_ANONYMOUS_ID, id);
+    }
+  } catch (error) {
+    console.error("There was an error sending identify data to APM:", error);
+  }
+}
 
 async function sendPageEvent(): Promise<void> {
   const data = {
     user_id: null,
     path: window.location.href,
     referrer: document.referrer,
-    ajs_anonymous_id: localStorage.getItem(AJS_ANONYMOUS_ID)
+    ajs_anonymous_id: localStorage.getItem(AJS_ANONYMOUS_ID),
   };
 
   try {
-    const res = await axios.post(`${ANALYTICS_URL}/page`, data, {withCredentials: true});
+    const res = await axios.post(`${ANALYTICS_URL}/page`, data, {
+      withCredentials: true,
+    });
     const id = res.data[AJS_ANONYMOUS_ID];
     if (id) {
       localStorage.setItem(AJS_ANONYMOUS_ID, id);
     }
   } catch (error) {
-    console.error('There was an error sending identify data to APM:', error);
+    console.error("There was an error sending identify data to APM:", error);
   }
 }
 
-export default module
+export default module;
