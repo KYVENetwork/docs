6643a7f92c88fa299bc8b63496edbacacbd23ae0
Troy Kessler <troy.kessler99@gmail.com>
2024-02-05 08:21:02 +0100

    fix: build

--- globalClientModule.ts
+++ globalClientModule.ts
@@ -1,11 +1,13 @@
-document.addEventListener("copy", () => {
-  const selection = document.getSelection();
+if (typeof window !== "undefined") {
+  document.addEventListener("copy", () => {
+    const selection = document.getSelection();
 
-  if (selection && (window as any).dw) {
-    (window as any).dw.track("Content Copied", {
-      text: selection.toString(),
-    });
-  }
-});
+    if (selection && (window as any).dw) {
+      (window as any).dw.track("Content Copied", {
+        text: selection.toString(),
+      });
+    }
+  });
+}
 
 export default module;
