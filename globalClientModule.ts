419a6fcf1453398bb82df8dbf89fd6534880ed52
GitInVisualStudio <kaminund@gmail.com>
2024-03-28 08:50:21 +0100

    update: add refiner

--- globalClientModule.ts
+++ globalClientModule.ts
@@ -1,14 +1,17 @@
+import _refiner from "refiner-js";
+
 if (typeof window !== "undefined") {
-    document.addEventListener("copy", () => {
-      const selection = document.getSelection();
-  
-      if (selection && (window as any).dw) {
-        (window as any).dw.track("Content Copied", {
-          text: selection.toString(),
-        });
-      }
-    });
-  }
-  
-  export default module;
-  
+  document.addEventListener("copy", () => {
+    const selection = document.getSelection();
+
+    if (selection && (window as any).dw) {
+      (window as any).dw.track("Content Copied", {
+        text: selection.toString(),
+      });
+    }
+  });
+
+  _refiner("setProject", "01b7c520-e1f0-11ee-b266-35f4ddac70ed");
+}
+
+export default module;
